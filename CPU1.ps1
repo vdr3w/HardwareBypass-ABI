e9c36f7a45f26170b0c6a386ed26f7ecf1fa763b
Drew <84882983+vdr3w@users.noreply.github.com>
2024-06-07 21:23:34 -0300

    Arquivo de powershell para mudança de GPU e CPU

---
+++ CPU1.ps1
@@ -0,0 +1,100 @@
+# Define the path to the log file
+$logFilePath = "C:\CPUNameChangeLog.txt"
+$logDir = [System.IO.Path]::GetDirectoryName($logFilePath)
+
+# Function to write output to the log file and console
+function Write-Log {
+    param (
+        [string]$message
+    )
+    if (!(Test-Path -Path $logDir)) {
+        New-Item -Path $logDir -ItemType Directory -Force
+    }
+    Add-Content -Path $logFilePath -Value $message
+    Write-Output $message
+}
+
+# Clear the log file at the beginning of the script
+if (Test-Path -Path $logFilePath) {
+    Clear-Content -Path $logFilePath -ErrorAction SilentlyContinue
+}
+
+# Solicitar ao usuário para coletar o caminho da instância do dispositivo
+Write-Log "Por favor, siga estes passos para coletar o Caminho da Instancia do Dispositivo do seu CPU:"
+Write-Log "1. Abra o Gerenciador de Dispositivos."
+Write-Log "2. Clique em 'Processadores'."
+Write-Log "3. De um duplo clique em qualquer um dos processadores listados."
+Write-Log "4. Va para a aba 'Detalhes'."
+Write-Log "5. No menu suspenso 'Propriedade', selecione 'Caminho da Instancia do Dispositivo'."
+Write-Log "6. Copie o valor exibido abaixo."
+
+
+# Prompt the user to paste the Device Instance Path
+$deviceInstancePath = Read-Host -Prompt "Cole o 'Caminho da Instancia do Dispositivo' aqui japoneis do carai"
+
+# Display the collected Device Instance Path
+Write-Log "Device Instance Path collected: $deviceInstancePath"
+
+# Define the new CPU name
+$newCpuName = "AMD Ryzen 7 5800X3D 8-Core Processor           "
+
+# Função para atualizar o nome da CPU no registro
+function UpdateCpuName {
+    param (
+        [string]$instancePath,
+        [string]$cpuName
+    )
+
+    # Remover a última parte do caminho do dispositivo que geralmente é uma subchave como '0'
+    $basePath = $instancePath.Substring(0, $instancePath.LastIndexOf("\"))
+    
+    $subKeys = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b'
+
+    foreach ($key in $subKeys) {
+        $regPath = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\$basePath\$key"
+
+        Write-Log "Atualizando nome da CPU no caminho do registro: $regPath"
+
+        try {
+            # Verificar se o caminho do registro existe
+            if (Test-Path -Path "Registry::$regPath") {
+                # Atualizar o valor FriendlyName
+                Set-ItemProperty -Path "Registry::$regPath" -Name "FriendlyName" -Value $cpuName -ErrorAction SilentlyContinue
+                Write-Log "Nome da CPU atualizado com sucesso para: $cpuName na subchave $key"
+            } else {
+                Write-Log "Caminho do registro não encontrado: $regPath"
+            }
+        } catch {
+            Write-Log "Erro ao acessar ou atualizar o caminho do registro: $regPath"
+        }
+    }
+}
+
+
+
+# Call the function to update the CPU name
+UpdateCpuName -instancePath $deviceInstancePath -cpuName $newCpuName
+
+$asciiArt = @"
+------------------------------------------------
+"ACABOU JAPONES, BORA TORCER. Meu pipi no seu popo"
+
+   
+                                         ____    _     __     _    ____
+                                        |####`--|#|---|##|---|#|--'##|#|
+      _                                 |____,--|#|---|##|---|#|--.__|_|
+    _|#)_____________________________________,--'EEEEEEEEEEEEEE'_=-.
+   ((_____((_________________________,--------[JW](___(____(____(_==)        _________
+                                  .--|##,----o  o  o  o  o  o  o__|/`---,-,-'=========`=+==.
+                                  |##|_Y__,__.-._,__,  __,-.___/ J \ .----.#############|##|
+                                  |##|              `-.|#|##|#|`===l##\   ___\############|##| [DREW CODIGOS]
+                                 =======-===l          |_|__|_|     \##`-"___,=======.###|##|
+                                                                     \__,"          '======'
+   
+      
+"ACABOU JAPONES, BORA TORCER. Meu pipi no seu popo"
+------------------------------------------------
+
+
+"@
+Write-Output $asciiArt
---
+++ GPU1 - Revert.ps1
@@ -0,0 +1,134 @@
+# Define the path to the log file
+$logFilePath = "C:\GPUlog.txt"
+$logDir = [System.IO.Path]::GetDirectoryName($logFilePath)
+
+# Function to write output to the log file and console
+function Write-Log {
+    param (
+        [string]$message
+    )
+    if (!(Test-Path -Path $logDir)) {
+        New-Item -Path $logDir -ItemType Directory -Force
+    }
+    Add-Content -Path $logFilePath -Value $message
+    Write-Output $message
+}
+
+# Clear the log file at the beginning of the script
+if (Test-Path -Path $logFilePath) {
+    Clear-Content -Path $logFilePath -ErrorAction SilentlyContinue
+}
+
+# Original GPU model to be replaced
+$oldGpuModel = "NVIDIA GeForce GTX 2060 SUPER"
+
+# New GPU model name for simulation
+$newGpuModel = "NVIDIA GeForce GTX 1660 SUPER"
+
+# Specific registry paths and value names to modify
+$registryPaths = @{
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinSAT" = @("PrimaryAdapterString");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0001" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0002" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0003" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Video\{850000A1-ADE9-11EE-927A-806E6F6E6963}\Video" = @("DeviceDesc");
+    "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\nvlddmkm\Video" = @("DeviceDesc");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Video\{850000A1-ADE9-11EE-927A-806E6F6E6963}\0001" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Video\{850000A1-ADE9-11EE-927A-806E6F6E6963}\0002" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Video\{850000A1-ADE9-11EE-927A-806E6F6E6963}\0003" = @("DriverDesc", "HardwareInformation.AdapterString", "HardwareInformation.ChipType");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Video\{850000A1-ADE9-11EE-927A-806E6F6E6963}\Video" = @("DeviceDesc");
+    "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\nvlddmkm\Video" = @("DeviceDesc");
+}
+
+# Function to search and replace the GPU name in registry values
+function SearchAndReplaceRegistryForGPU {
+    param (
+        [string]$oldGpuName,
+        [string]$newGpuName,
+        [hashtable]$paths
+    )
+
+    $notFoundPaths = @()
+    $alreadyModifiedPaths = @()
+
+    foreach ($path in $paths.Keys) {
+        foreach ($valueName in $paths[$path]) {
+            Write-Log "Checking Path: $path - Value: $valueName"
+            try {
+                if (Test-Path -Path "Registry::$path") {
+                    $value = Get-ItemProperty -Path "Registry::$path" -Name $valueName -ErrorAction SilentlyContinue
+                    if ($null -ne $value) {
+                        if ($value.$valueName -is [string]) {
+                            if ($value.$valueName -like "*$newGpuName*") {
+                                Write-Log "Already modified: Path: $path - Property: $valueName - Data: $($value.$valueName)"
+                                $alreadyModifiedPaths += "$path - Property: $valueName - Data: $($value.$valueName)"
+                            } elseif ($value.$valueName -like "*$oldGpuName*") {
+                                $newValue = $value.$valueName -replace [regex]::Escape($oldGpuName), $newGpuName
+                                Set-ItemProperty -Path "Registry::$path" -Name $valueName -Value $newValue -ErrorAction SilentlyContinue
+                                Write-Log "Replaced value: Path: $path - Property: $valueName - Data: $newValue"
+                            } else {
+                                Write-Log "No match found: Path: $path - Property: $valueName - Data: $($value.$valueName)"
+                            }
+                        } else {
+                            Write-Log "Value not found: Path: $path - Property: $valueName"
+                            $notFoundPaths += "$path - Property: $valueName"
+                        }
+                    } else {
+                        Write-Log "Value not found: Path: $path - Property: $valueName"
+                        $notFoundPaths += "$path - Property: $valueName"
+                    }
+                } else {
+                    Write-Log "Path not found: $path"
+                    $notFoundPaths += "$path"
+                }
+            } catch {
+                Write-Log "Error accessing path: $path - Property: $valueName"
+            }
+        }
+    }
+
+    Write-Log "`n### Paths not found ###"
+    $notFoundPaths | ForEach-Object { Write-Log $_ }
+
+    Write-Log "`n### Paths already modified ###"
+    $alreadyModifiedPaths | ForEach-Object { Write-Log $_ }
+}
+
+# Search and replace the registry entries for the GPU name
+Write-Log "Commencing registry search and replace operation..."
+SearchAndReplaceRegistryForGPU -oldGpuName $oldGpuModel -newGpuName $newGpuModel -paths $registryPaths
+
+
+$asciiArt = @"
+------------------------------------------------
+"GPU TA REVERTIDA JAPONES, AGORA PODE ABRIR O SCRIPT DO PROCESSADOR"
+
+                                      ___
+   
+                                    ./  _)                                               O
+                             _.---._|____\__________________,-------.------------==-.____|]
+    ____                    /  ##-|=======--------===-----==-|-----=|=-------------._______)
+   (####L-.________________/  /___|__________________________ ______|               )-------.
+    \###|  ________________) `--- |---------------------===== ,_____|         _____/========"
+     |##F-'              | /\ --- |         __mmmmm__________(======|...---''/
+     |###/               (_\ \    |_______ ' "-L  nn [mf]    |      |
+     |##|                     """"        \__________________|      |
+    /##/                                   )       //((  // ||   _,-/
+   /##/                                   /       /\\_V_//   |,-"  (
+   \#/                                  ./       /  `---"     \     \
+    v                                  /_       /              \     `.
+                                      /_ `-.___/                \      `.
+                                        `-.___/                  ".      `.        [DREW CODIGOS]
+                                                                   ".      `.
+                                                                     ".    _,"
+                                                                       ".-"
+   
+"GPU TA REVERTIDA JAPONES, AGORA PODE ABRIR O SCRIPT DO PROCESSADOR"
+------------------------------------------------
+
+
+"@
+Write-Output $asciiArt
+
